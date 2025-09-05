31ed05f569c7305a68c6c5f4af862dcf95642c99
Alex <121983148+vindepemarte@users.noreply.github.com>
2025-09-06 00:07:56 +0200

    Fix tutorial video modal: make responsive and prevent overflow

--- src/components/modals/tutorial-modal.tsx
+++ src/components/modals/tutorial-modal.tsx
@@ -11,12 +11,20 @@ interface TutorialModalProps {
 export default function TutorialModal({ open, onOpenChange }: TutorialModalProps) {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="sm:max-w-3xl">
-        <DialogHeader>
+      <DialogContent className="max-w-[95vw] max-h-[95vh] w-full h-full p-0">
+        <DialogHeader className="p-6 pb-4">
           <DialogTitle>How to Request a New Homework</DialogTitle>
         </DialogHeader>
-        <div className="aspect-video">
-          <video className="w-full" src="/tutorial.mp4" controls />
+        <div className="flex-1 px-6 pb-6">
+          <div className="aspect-video w-full h-full max-h-[calc(95vh-120px)]">
+            <video 
+              className="w-full h-full object-contain rounded-lg" 
+              src="/tutorial.mp4" 
+              controls 
+              controlsList="nodownload"
+              preload="metadata"
+            />
+          </div>
         </div>
       </DialogContent>
     </Dialog>
